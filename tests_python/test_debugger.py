905d800a26224497d02768c7f58a8316436a4da2
fabioz <fabiofz@gmail.com>
2019-03-23 09:05:40 -0300

    wip

--- tests_python/test_debugger.py
+++ tests_python/test_debugger.py
@@ -2340,22 +2340,33 @@ def test_subprocess_quoted_args(case_setup_multiprocessing):
 
             def run(self):
                 from tests_python.debugger_unittest import ReaderThread
-                server_socket.listen(1)
-                self.server_socket = server_socket
-                new_sock, addr = server_socket.accept()
+                # Note: on linux on Python 2 because on Python 2 CPython subprocess.call will actually
+                # create a fork first (at which point it'll connect) and then, later on it'll
+                # call the main (as if it was a clean process as if PyDB wasn't created
+                # the first time -- the debugger will still work, but it'll do an additional
+                # connection.
 
-                reader_thread = ReaderThread(new_sock)
-                reader_thread.name = '  *** Multiprocess Reader Thread'
-                reader_thread.start()
+                expected_connections = 1
+                if sys.platform != 'win32' and IS_PY2:
+                    expected_connections = 2
 
-                writer2 = SecondaryProcessWriterThread()
+                for _ in range(expected_connections):
+                    server_socket.listen(1)
+                    self.server_socket = server_socket
+                    new_sock, addr = server_socket.accept()
 
-                writer2.reader_thread = reader_thread
-                writer2.sock = new_sock
+                    reader_thread = ReaderThread(new_sock)
+                    reader_thread.name = '  *** Multiprocess Reader Thread'
+                    reader_thread.start()
 
-                writer2.write_version()
-                writer2.write_add_breakpoint(break_subprocess_line)
-                writer2.write_make_initial_run()
+                    writer2 = SecondaryProcessWriterThread()
+
+                    writer2.reader_thread = reader_thread
+                    writer2.sock = new_sock
+
+                    writer2.write_version()
+                    writer2.write_add_breakpoint(break_subprocess_line)
+                    writer2.write_make_initial_run()
                 hit = writer2.wait_for_breakpoint_hit()
                 writer2.write_run_thread(hit.thread_id)
 
