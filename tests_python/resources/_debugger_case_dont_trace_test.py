d8db879b9a13ebd40d467731180e0881a63ab845
fabioz <fabiofz@gmail.com>
2019-04-16 17:15:59 -0300

    wip

--- tests_python/resources/_debugger_case_dont_trace_test.py
+++ tests_python/resources/_debugger_case_dont_trace_test.py
@@ -1,6 +1,9 @@
-from _debugger_case_dont_trace import call_me_back
+from ._debugger_case_dont_trace import call_me_back
+
+
 def my_callback():
     print('trace me')  # Break here
+
 
 if __name__ == '__main__':
     call_me_back(my_callback)
